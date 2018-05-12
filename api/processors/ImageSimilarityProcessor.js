e2376152b3ea61f9a5dab3eec975786bb971b90a
Kartik <ksomani@amazon.com>
2018-05-12 19:34:22 +0530

    Adding title and message to image similarity processor

--- api/processors/ImageSimilarityProcessor.js
+++ api/processors/ImageSimilarityProcessor.js
@@ -6,20 +6,31 @@ var processFiles = function (files, data) {
 		var path = files[i].fd
 		var name = getFileName(path)
 		shell.exec("cd /Users/$USER/SafetHackthon/ && python3 classify_images_updated.py " + path + "&& python3 cluster_vectors.py")
+        try {
         var temp = JSON.parse(fs.readFileSync("/Users/sarrafp/SafetHackthon/nearest_neighbors/" + name + ".json", "utf-8"))
+        } catch (err) {
+            console.error("It seems that similarity magic failed to complete.")
+            temp = [];
+        }
         var finalResult = []
         for(var row of temp) {
         	if(row.filename !== name) {
         		finalResult.push(row)
         	}
         }
-        console.log(files[i])
+        var riskClass = sails.config.hackathon.riskClasses.none;
+        var riskTitle = "Image Similarity";
+        var riskMessage = "Didn't find any similar image"
+        //TODO change risk class and message  based on similarity % of highest match
         response.push({
         	filename: files[i].filename,
-            similarity: finalResult
+            similarity: finalResult,
+            riskClass: riskClass,
+            riskTitle: riskTitle,
+            riskMessage: riskMessage,
+            processName: "imageSimilarity",
         })
 	}
-	console.log(response)
 
 	return {fileResults: response};
 
