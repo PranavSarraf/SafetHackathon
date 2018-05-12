d0e4fc7651ddf824367560b3209e047fd0cf45b1
Sarraf <sarrafp@amazon.com>
2018-05-12 20:05:28 +0530

    Made path relative and optimized the process

--- api/processors/ImageSimilarityProcessor.js
+++ api/processors/ImageSimilarityProcessor.js
@@ -2,26 +2,38 @@ var processFiles = function (files, data) {
 	const shell = require('shelljs');
 	const fs = require('fs')
 	var response = [];
+
+	for(var file of files) {
+		var path = file.fd
+		var name = getFileName(path)
+		shell.exec("python3 classify_images_updated.py " + path)
+		console.log("Done classify_images_updated!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	}
+	console.log("Custering Running!!!!!!!!!!!!")
+	shell.exec("python3 cluster_vectors.py")
+	console.log("Custering Done!!!!!!!!!!!!")
 	for (var i = files.length - 1; i >= 0; i--) {
 		var path = files[i].fd
 		var name = getFileName(path)
-		shell.exec("cd /Users/$USER/SafetHackthon/ && python3 classify_images_updated.py " + path + "&& python3 cluster_vectors.py")
-        try {
-        var temp = JSON.parse(fs.readFileSync("/Users/sarrafp/SafetHackthon/nearest_neighbors/" + name + ".json", "utf-8"))
+		try {
+		var temp = JSON.parse(fs.readFileSync("./nearest_neighbors/" + name + ".json", "utf-8"))
         } catch (err) {
             console.error("It seems that similarity magic failed to complete.")
             temp = [];
-        }
+        }	
         var finalResult = []
         for(var row of temp) {
         	if(row.filename !== name) {
         		finalResult.push(row)
+        		console.log(JSON.stringify(row))
         	}
         }
         var riskClass = sails.config.hackathon.riskClasses.none;
         var riskTitle = "Image Similarity";
         var riskMessage = "Didn't find any similar image"
         //TODO change risk class and message  based on similarity % of highest match
+        console.log(files[i])
+        console.log(JSON.stringify(finalResult))
         response.push({
         	filename: files[i].filename,
             similarity: finalResult,
@@ -31,9 +43,9 @@ var processFiles = function (files, data) {
             processName: "imageSimilarity",
         })
 	}
+	console.log(JSON.stringify(response))
 
 	return {fileResults: response};
-
 }
 
 function getFileName(path) {
